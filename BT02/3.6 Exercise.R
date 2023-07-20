9bad3ac5a80d53ccfdf19e3ee5e4ffe2fa4b2712
Monet <minhthang421992@gmail.com>
2023-07-21 00:08:33 +0700

    Ver2

---
+++ BT02/3.6 Exercise.R
@@ -0,0 +1,10 @@
+# Find what arguments the sample() function takes. We will need that for later.
+
+# Find out what arguments the median function takes. While calling the median for the lvl object, try setting the na.rm = argument to TRUE. 
+
+# Does R return a different value from the one when na.rm = is set to FALSE (NB: the default value for the na.rm = argument is FALSE; this means that whenever we do not explicitly specify that it must be set to TRUE, it is set to FALSE)? 
+# Yes, when the na.rm argument of a function in R is set to FALSE (which is the default value if not specified), it means that the function will not remove any missing values (NA) from the calculation.
+  
+# Now try the same command but with the atk variable. 
+
+# Is the output different? Why do you think that is?
