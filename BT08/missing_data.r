3a5f52bcfe2ee165ba54aade05f4e06721b7605c
Monet <minhthang421992@gmail.com>
2023-08-14 18:31:08 +0700

    Updated

--- BT08/missing_data.r
+++ BT08/missing_data.r
@@ -1,6 +1,6 @@
 # Import the `readr` package
 library(readr)
 
-path = "F:\R-Aptech\BT08\employee_data_na.csv"
+path = "F:\\R-Aptech\\BT08\\employee_data_na.csv"
 # Read the `employee_data_na.csv` file
 employee_data <- read_csv(path)
